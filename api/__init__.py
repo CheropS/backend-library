079fd8f640c150739be1553660565d745650d99e
Kennedy-Mwaura <kemwaura@gmail.com>
2021-09-29 14:11:29 +0300

    Updated the main init file

---
+++ api/__init__.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+from config import app_config
+
+app = Flask('__name__')
+
+CORS(app)
+jwt = JWTManager(app)
+app.config.from_object(app_config['development'])
+app.url_map.strict_slashes = False
+
+db = SQLAlchemy(app)
